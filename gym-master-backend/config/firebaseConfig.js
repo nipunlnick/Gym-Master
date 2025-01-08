baebfe7b688e491858ac683d4b3a12b5a0af0fce
Nipun Lakshitha <pvnipunlakshitha@gmail.com>
2025-01-08 23:58:20 +0530

    refactor: update Firebase configuration

--- gym-master-backend/config/firebaseConfig.js
+++ gym-master-backend/config/firebaseConfig.js
@@ -1,5 +1,5 @@
 import admin from 'firebase-admin';
-import { createRequire } from 'module'; // Needed to use require in ES Modules
+import { createRequire } from 'module';
 
 // Use `createRequire()` to load JSON files in ES Module context
 const require = createRequire(import.meta.url);
@@ -9,9 +9,7 @@ const serviceAccount = require('./firebaseAdminConfig.json');
 if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
-        //databaseURL: "https://gym-master-ff735.firebaseio.com",  // Ensure this is your actual database URL
-        //databaseURL: "https://gym-master-65885.firebaseio.com",
-        databaseURL: "https://gym-main-36133.firebaseio.com",
+        databaseURL: "https://gym-master-ff735.firebaseio.com",
     });
 }
 
